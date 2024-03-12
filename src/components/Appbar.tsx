dd9d27405010f8ef62b6a3d3e226f70cf92a65ad
felipe <jonathanfelipe0111@hotmail.com>
2024-03-12 18:19:50 -0500

    feat: añadiendo nabar con nextUI
    Utilizando la liberia de nextUI para crear una barra de navegacion temporalmente.
    posteriormente se cambiara

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import SigninButton from "./SigninButton";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+
 
 const Appbar = () => {
   return (
-    <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow">
-      <SigninButton />
-    </header>
+    <>
+      <Navbar shouldHideOnScroll>
+        <NavbarBrand>
+          <p className="font-bold text-inherit">SKATES-CO</p>
+        </NavbarBrand>
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarItem>
+            <Link color="foreground" href="#">
+              Top skates
+            </Link>
+          </NavbarItem>         
+        </NavbarContent>
+        <NavbarContent justify="end">    
+          <NavbarItem>
+            <Button as={Link} color="primary" href="#" variant="flat">
+              <SigninButton />
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </>
   );
 };
 
