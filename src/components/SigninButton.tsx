6b9a43cefd63ec5a13925fb1ceb9be65866481b4
cristito <cdpsk8@gmail.com>
2024-04-12 15:02:36 -0500

    boton completar registro

--- src/components/SigninButton.tsx
+++ src/components/SigninButton.tsx
@@ -2,21 +2,31 @@
 import { useState } from 'react';
 import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect } from 'react';
 
 const SigninButton = () => {
+  const [porfileComplete, setProfileComplete] = useState(false);
   const { data: session } = useSession();
   const [openModal, setModal] = useState(false);
   const handleModal = () => {
     setModal(!openModal)
   }
+
   const [formData, setFormData] = useState({
     name: '',
     foto: '',
+    phone: '',
+    location: '',
+    biografia: '',
     redes: '',
     score: 0,
-    email: '',
+    email: session?.user?.email || 'prueba@prueba.com',
     team_id: 1
   });
+
+  const preRegister =  () => {
+    console.log("pre Registro");
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -27,6 +37,7 @@ const SigninButton = () => {
     console.log(value);
   };
 
+  //completar registro
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -36,8 +47,8 @@ const SigninButton = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-/*           'X-Requested-With':
-            'XMLHttpRequest' */
+          /*           'X-Requested-With':
+                      'XMLHttpRequest' */
           // Si necesitas enviar algún token de autenticación, aquí es donde lo incluirías
           // 'Authorization': 'Bearer ' + token,
         },
@@ -46,6 +57,8 @@ const SigninButton = () => {
 
       const responseData = await response.json();
       console.log("registro completado", responseData);
+      setProfileComplete(true);
+      handleModal();
       // Aquí puedes manejar la respuesta del servidor según lo necesites
     } catch (error) {
       console.error('Error:', error);
@@ -53,14 +66,67 @@ const SigninButton = () => {
     }
   };
 
+  //confirmar correo
+
+  useEffect(() => {
+    const checkProfile = async () => {
+      if (session && session.user && session.user.email) {
+        try {
+          const isComplete = await getSkate(session.user.email);
+          console.log(isComplete)
+          //pregnta si el estado del skate esta completo mostrar boton "ver perfil"
+          //pregnta si el estado del skate no esta completo mostrar boton "completar registro"
+          setProfileComplete(isComplete);
+        } catch (error) {
+          console.error('Error al verificar el perfil:', error);
+          // En caso de error, establecer profileComplete en false
+          setProfileComplete(false);
+        }
+      }
+    };
+
+    checkProfile();
+  }, [session]);
+
+  const getSkate = async (email:String) => {
+    console.log(email)
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/search-by-email?email=${email}`, {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      
+      const responseData = await response.json();
+      // Verificar si el perfil está completo
+      return responseData; // Si responseData no está vacío, el perfil está completo
+    } catch (error) {
+      console.error('Error:', error);
+      throw error; // Lanzar error para manejarlo en el useEffect
+    }
+  };
+
+
+
+
   if (session && session.user) {
     return (
       <div className="flex gap-4 ml-auto">
-        <button
+        {
+          porfileComplete ? 
+          <button
           type='button'
           className='h-10 px-4 font-medium text-sm rounded-md text-white bg-gray-900'
-          onClick={handleModal}
-        >completar registro</button>
+          onClick={()=>console.log("ver perfil")}
+        >ver perfil</button> :
+           <button
+            type='button'
+            className='h-10 px-4 font-medium text-sm rounded-md text-white bg-gray-900'
+            onClick={handleModal}
+          >completar registro</button>
+        }
+
         {
           openModal &&
           <div className='fixed top-0 left-0 w-full h-full bg-gray-300 flex justify-center items-center'>
@@ -68,7 +134,7 @@ const SigninButton = () => {
               <h2 className='text-sm font-medium text-gray-900 border-b border-gray-300 py-3 px-4 mb-4'>Completa tu registro</h2>
               <div className='px-4 pb-4'>
                 <div className="p-4 md:p-5 space-y-4">
-                  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+                  <form className="flex grid grid-cols-2 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 " onSubmit={handleSubmit}>
                     <div className="mb-4">
                       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Name:</label>
                       <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -89,6 +155,36 @@ const SigninButton = () => {
                         onChange={handleChange}
                       />
                     </div>
+                    <div className="mb-4">
+                      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="foto">phone:</label>
+                      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="number"
+                        id="phone"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="foto">location:</label>
+                      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        id="location"
+                        name="location"
+                        value={formData.location}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="foto">biografia:</label>
+                      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        id="biografia"
+                        name="biografia"
+                        value={formData.biografia}
+                        onChange={handleChange}
+                      />
+                    </div>
                     <div>
                       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="redes">Redes:</label>
                       <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -99,19 +195,9 @@ const SigninButton = () => {
                         onChange={handleChange}
                       />
                     </div>
-                    <div>
-                      <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="redes">Mail:</label>
-                      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="text"
-                        id="email"
-                        name="email"
-                        value={session && session.user && session.user.email ? session.user.email : ""}
-                        onChange={handleChange}
-                      />  
-                    </div>
                     <div className="flex items-center justify-between">
                       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Enviar</button>
-                    </div>
+                      ¡                    </div>
                   </form>
                 </div>
               </div>
@@ -126,7 +212,7 @@ const SigninButton = () => {
                 </button>
               </div>
             </div>
-          </div>  
+          </div>
         }
 
 
