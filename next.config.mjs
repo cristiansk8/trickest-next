50c9d48f7825b487461af2b240dd79ecea4bbdc5
cristito <cdpsk8@gmail.com>
2024-06-07 00:34:19 -0500

    eslint

--- next.config.mjs
+++ next.config.mjs
@@ -2,26 +2,26 @@
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-},
+  },
 
-    images: {
-       
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'images.unsplash.com',
-            },
-            {
-              protocol: 'https',
-              hostname: 'raw.githubusercontent.com',
-            },
-            {
-              protocol: 'https',
-              hostname: 'tailone.tailwindtemplate.net',
-            },
-            
-          ],
-    }
+  images: {
+
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tailone.tailwindtemplate.net',
+      },
+
+    ],
+  }
 };
 
 export default nextConfig;
