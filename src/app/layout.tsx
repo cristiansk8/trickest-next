d051f8cba0955561160b601cb688cab6ceac2dac
cristito <cdpsk8@gmail.com>
2024-08-08 14:43:59 -0500

    navbar out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={urbanist.className}>
         <Header/>
-        <Navbar/>  
+        
         {children}
       </body>
     </html>
