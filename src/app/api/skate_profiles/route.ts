4a3d11b40990d1e011a79bc3558b98ab0a0ff3b8
felipe <jonathanfelipe0111@hotmail.com>
2025-02-06 14:52:57 -0500

    phone undefine

--- src/app/api/skate_profiles/route.ts
+++ src/app/api/skate_profiles/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: Request) {
             data: {
                 email,
                 name,
-                phone,
+                phone: phone ?? undefined,
                 createdAt: new Date(),
             },
         });
